42e31597976afca807e6f95452559e779f7762df
Igor Ferreira <me@igorcferreira.dev>
2026-03-14 19:37:04 +0100

    Remove un-necessary icon handling

--- App/TransactionHistoryApp/TransactionHistoryApp.swift
+++ App/TransactionHistoryApp/TransactionHistoryApp.swift
@@ -10,10 +10,6 @@ import SwiftData
 import TransactionHistory
 import AppIntents
 
-#if os(macOS)
-import AppKit
-#endif
-
 extension ProcessInfo {
     static let isTestEnvironmentKey = "IS_UI_TESTING"
 
@@ -28,11 +24,6 @@ extension ProcessInfo {
 struct TransactionHistoryApp: App {
     let dataStorage = DataStorage()
 
-    #if os(macOS)
-    // Observes system appearance changes to switch the Dock icon
-    @Environment(\.colorScheme) private var colorScheme
-    #endif
-
     var container: ModelContainer {
         if ProcessInfo.isTest {
             DataStorage.createMockEnvironment()
@@ -43,10 +34,6 @@ struct TransactionHistoryApp: App {
 
     init() {
         TransactionHistoryProvider.updateAppShortcutParameters()
-        #if os(macOS)
-        updateMacOSIcon()
-        observeAppearanceChanges()
-        #endif
     }
 
     var body: some Scene {
@@ -59,28 +46,4 @@ struct TransactionHistoryApp: App {
         .modelContainer(container)
         #endif
     }
-
-    #if os(macOS)
-    /// Updates the macOS Dock icon based on the current system appearance.
-    private func updateMacOSIcon() {
-        let isDark = NSApp?.effectiveAppearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
-        if isDark {
-            NSApp?.applicationIconImage = NSImage(named: "AppIcon-Dark")
-        } else {
-            // nil resets to the default app icon
-            NSApp?.applicationIconImage = nil
-        }
-    }
-
-    /// Observes system appearance changes to keep the Dock icon in sync.
-    private func observeAppearanceChanges() {
-        DistributedNotificationCenter.default().addObserver(
-            forName: Notification.Name("AppleInterfaceThemeChangedNotification"),
-            object: nil,
-            queue: .main
-        ) { _ in
-            updateMacOSIcon()
-        }
-    }
-    #endif
 }
